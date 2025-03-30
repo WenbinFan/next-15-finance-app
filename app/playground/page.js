6581a2505ec8b1bef4372bce10fe71795baeb4d7
Bowen <bowenfan621@gmail.com>
2025-03-30 16:56:30 -0400

    99. Financial Trend Component

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,4 +1,5 @@
 import PageHeader from "@/components/page-header";
+import Trend from "@/components/trend";
 
 export default function Page() {
   return (
@@ -6,9 +7,20 @@ export default function Page() {
         <h1 className="text-4xl mt-8">Playground</h1>
 
         <div>
-            <h2 className="mb-4 text-lg font-mono">ComponentName</h2>
+            <h2 className="mb-4 text-lg font-mono">PageHeader</h2>
             <hr className="b-4 border-gray-200 dark:border-gray-800"/>
             <div><PageHeader /></div>
+        </div>
+
+        <div>
+            <h2 className="mb-4 text-lg font-mono">Trend</h2>
+            <hr className="b-4 border-gray-200 dark:border-gray-800"/>
+            <div className="flex space-x-4">
+              <Trend type="Income" amount={1000} />
+              <Trend type="Expense" amount={12000} />
+              <Trend type="Investment" amount={7000} />
+              <Trend type="Saving" amount={500} />
+            </div>
         </div>
     </main>
   )
