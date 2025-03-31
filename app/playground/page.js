5500633e235c3aea793c4dee6e9c5f5cea3b979c
Bowen <bowenfan621@gmail.com>
2025-03-31 16:17:28 -0400

    102. Transaction Item Component

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,4 +1,5 @@
 import PageHeader from "@/components/page-header";
+import TransactionItem from "@/components/transaction-item";
 import Trend from "@/components/trend";
 
 export default function Page() {
@@ -14,12 +15,23 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">Trend</h2>
-            <hr className="b-4 border-gray-200 dark:border-gray-800"/>
+            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
             <div className="flex space-x-4">
               <Trend type="Income" amount={1000} prevAmount={900} />
               <Trend type="Expense" amount={12000} prevAmount={10000} />
               <Trend type="Investment" amount={7000} prevAmount={11100} />
               <Trend type="Saving" amount={500} prevAmount={950} />
+            </div>
+        </div>
+
+        <div>
+            <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
+            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <div className="space-y-4">
+              <TransactionItem type="Income" description="Salary" amount={2000} />
+              <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+              <TransactionItem type="Saving" description="For children" amount={500} />
+              <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
             </div>
         </div>
     </main>
