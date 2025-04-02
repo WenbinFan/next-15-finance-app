509f698804a7fe3940a86b3bd088a4b950b49ecf
Bowen <bowenfan621@gmail.com>
2025-04-02 14:40:58 -0400

    112. Separator & Loading Skeleton

--- app/playground/page.js
+++ app/playground/page.js
@@ -6,6 +6,8 @@ import Button from "@/components/button";
 import Label from "@/components/label";
 import Input from "@/components/input";
 import Select from "@/components/select";
+import Separator from "@/components/separator";
+import Skeleton from "@/components/skeleton";
 
 export default function Page() {
   return (
@@ -14,13 +16,13 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">PageHeader</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div><PageHeader /></div>
         </div>
 
         <div>
             <h2 className="mb-4 text-lg font-mono">Trend</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div className="flex space-x-4">
               <Trend type="Income" amount={1000} prevAmount={900} />
               <Trend type="Expense" amount={12000} prevAmount={10000} />
@@ -31,7 +33,7 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div className="space-y-4">
               <TransactionItem type="Income" description="Salary" amount={2000} />
               <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
@@ -42,7 +44,7 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">TransactionSummaryItem + TransactionItem</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div className="space-y-4">
               <TransactionSummaryItem date="2025-03-31" amount={3500} />
               <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
@@ -55,7 +57,7 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">Buttons</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div className="space-x-4">
               <Button>Hello</Button>
               <Button variant="outline">Hello</Button>
@@ -69,7 +71,7 @@ export default function Page() {
 
         <div>
             <h2 className="mb-4 text-lg font-mono">Forms</h2>
-            <hr className="mb-4 border-gray-200 dark:border-gray-800"/>
+            <Separator />
             <div className="grid grid-cols-2 gap-4">
               <div>
                 <Label>Yout name</Label>
@@ -89,6 +91,24 @@ export default function Page() {
               <div className="flex items-center">
                 <Input type="checkbox" id="terms"/>
                 <Label className="ml-2" htmlFor="terms">Accept the terms</Label>
+              </div>
+            </div>
+        </div>
+
+        <div>
+            <h2 className="mb-4 text-lg font-mono">Loading Skeleton</h2>
+            <Separator />
+            <div className="space-y-8">
+              <div className="flex space-x-4">
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+              </div>
+
+              <div className="space-y-4">
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
               </div>
             </div>
         </div>
