fe67537e442a850303bbb5b036c21aa708b0b891
Bowen <bowenfan621@gmail.com>
2025-04-03 00:58:51 -0400

    122. Adding a Zod Schema for Transactions

--- app/dashboard/components/transaction-form.js
+++ app/dashboard/components/transaction-form.js
@@ -5,7 +5,8 @@ import Input from "@/components/input";
 import Button from "@/components/button";
 import { categories,types } from "@/lib/consts" 
 import { useForm } from "react-hook-form"
-import { data } from "autoprefixer";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { transactionSchema } from "@/lib/validation"
 
 export default function TransactionForm() {
     const {
@@ -14,7 +15,8 @@ export default function TransactionForm() {
         watch,
         formState: { errors },
       } = useForm({
-        mode: "onTouched"
+        mode: "onTouched",
+        resolver: zodResolver(transactionSchema)
       })
 
       const onSubmit = (data) => console.log(data)
@@ -37,23 +39,19 @@ export default function TransactionForm() {
 
         <div>
             <Label className="mb-1">Transaction Date</Label>
-            <Input {...register("created_at", {required: "The date is required"})}/>
+            <Input {...register("created_at")}/>
             {errors.created_at && <p className="mt-1 text-red-500">{errors.created_at.message}</p>}
         </div>
 
         <div>
             <Label className="mb-1">Amount</Label>
-            <Input type="number" {...register("amount", {
-                required: "The amount is required",
-                valueAsNumber: true,
-                min: {value: 1, message: "The amount must be greater than 1"},
-                })} />
+            <Input type="number" {...register("amount")} />
             {errors.amount && <p className="mt-1 text-red-500">{errors.amount.message}</p>}
         </div>
 
         <div className="col-span-1 md:col-span-2">
             <Label className="mb-1">Description</Label>
-            <Input type="number" {...register("description", {required: "The description is required"})} />
+            <Input type="number" {...register("description")} />
             {errors.description && <p className="mt-1 text-red-500">{errors.description.message}</p>}
         </div>
     </div>
