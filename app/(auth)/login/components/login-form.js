0deb02056436270e24ce624bb20bbede4df3278c
Bowen <bowenfan621@gmail.com>
2025-04-06 15:35:33 -0400

    164. Handling Form Server Action Errors

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -3,10 +3,20 @@
 import SubmitButton from "@/components/submit-button";
 import Input from "@/components/input";
 import { login } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
+const initialState = {
+    message: "",
+    error: false,    
+}
 
 export default function LoginForm() {
-  return <form action={login} className="space-y-2">
+    const [state, formAction] = useFormState(login, initialState)
+  return <form action={formAction} className="space-y-2">
     <Input type="email" name="email" placeholder="name@example.com" required />
     <SubmitButton type="submit" size="sm" className="w-full">Sign in with email</SubmitButton>
+    <p className={`${state?.error ? 'text-red-500' : 'text-green-500'} text-sm text-center`}>
+        {state.message}
+    </p>
   </form>
 }
