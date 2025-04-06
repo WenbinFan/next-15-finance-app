49694d0c6cd4ca8145d2050ac725064014a2ed15
Bowen <bowenfan621@gmail.com>
2025-04-06 15:22:01 -0400

    163. Form Pending State

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -1,10 +1,12 @@
-import Button from "@/components/button";
+'use client'
+
+import SubmitButton from "@/components/submit-button";
 import Input from "@/components/input";
 import { login } from "@/lib/actions";
 
 export default function LoginForm() {
   return <form action={login} className="space-y-2">
     <Input type="email" name="email" placeholder="name@example.com" required />
-    <Button type="submit" size="sm" className="w-full">Sign in with email</Button>
+    <SubmitButton type="submit" size="sm" className="w-full">Sign in with email</SubmitButton>
   </form>
 }
