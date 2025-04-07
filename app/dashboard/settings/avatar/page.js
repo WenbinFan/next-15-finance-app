3eff6c4b30ffb7ddbe577dc7f2fa75ea4143d25a
Bowen <bowenfan621@gmail.com>
2025-04-07 16:14:05 -0400

    190. More Graceful Error Handling in Server Actions

--- app/dashboard/settings/avatar/page.js
+++ app/dashboard/settings/avatar/page.js
@@ -5,15 +5,22 @@ import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
 import { uploadAvatar } from "@/lib/actions";
 import { Ban, Check } from "lucide-react";
+import { useActionState } from "react";
+
+const initialState = {
+  message: "",
+  error: false,
+}
 
 export default function Page() {
+  const [state, formAction]  = useActionState(uploadAvatar, initialState);
   return <>
     <h1 className="text-4xl font-semibold mb-8">
       Avatar
     </h1>
-    <form className="space-y-4" action={uploadAvatar}>
-      <Alert icon={<Ban className="text-red-700 dark:text-red-300 w-6 h-6"/>} title={<span className="text-red-700 dark:text-red-300">Hello</span>}><span className="text-red-700 dark:text-red-300">Hello</span></Alert>
-      <Alert icon={<Check className="text-green-700 dark:text-green-300 w-6 h-6"/>} title={<span className="text-green-700 dark:green-red-300">Hello</span>}><span className="text-green-700 dark:text-green-300">Hello</span></Alert>
+    <form className="space-y-4" action={formAction}>
+      {state?.error && <Alert icon={<Ban className="text-red-700 dark:text-red-300 w-6 h-6"/>} title={<span className="text-red-700 dark:text-red-300">Error</span>}><span className="text-red-700 dark:text-red-300">{state?.message}</span></Alert>}
+      {!state?.error && state?.message.length > 0 && <Alert icon={<Check className="text-green-700 dark:text-green-300 w-6 h-6"/>} title={<span className="text-green-700 dark:green-red-300">Success</span>}><span className="text-green-700 dark:text-green-300">{state?.message}</span></Alert>}
       <Input type="file" name="file" id="file"/>
       <SubmitButton className="dark:bg-gray-300">Upload Avatar</SubmitButton>
     </form>
