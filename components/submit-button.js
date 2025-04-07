1b990363294f1304168d357b5a9ff6c99cc57ddc
Bowen <bowenfan621@gmail.com>
2025-04-07 15:15:41 -0400

    184. Image Uploading Form

--- components/submit-button.js
+++ components/submit-button.js
@@ -4,7 +4,7 @@ import { useFormStatus } from "react-dom"
 
 export default function Submit(props) {
     const { pending } = useFormStatus()
-    return <Button {...props} className={`${props.className} flex items-center justify-center space-x-2`}>
+    return <Button {...props} className={`${props.className} flex items-center justify-center space-x-2 px-4 py-2`}>
         {pending && <Loader className="animate-spin w-4 h-4" />}
         {props.children}
     </Button>
