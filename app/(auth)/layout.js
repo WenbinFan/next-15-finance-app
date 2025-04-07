acc33065bf3cd30bf27f0f4f269343a3ca42dbc3
Bowen <bowenfan621@gmail.com>
2025-04-06 23:24:34 -0400

    175. Protecting Routes in Middleware

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Layout({children}) {
   return <main>
     <div className="absolute left-8 top-8"></div>
-    <Link href="/" className={`${variants['ghost']} ${sizes['base']} flex items-center space-x-2 text-sm`}>
+    <Link href="/" className={`${variants['ghost']} ${sizes['base']} flex items-center space-x-2 text-sm mt-4`}>
         <ChevronLeft className="w-4 h-4" />
         <span>Back</span>
     </Link>
--- middleware.js
+++ middleware.js
@@ -1,7 +1,18 @@
-import { NextResponse } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
+import { createClient } from "./lib/supabase/server";
 
 export async function middleware(request) {
+    const supabase = await createClient()
+    const {data: { user }} = await supabase.auth.getUser()
+
+    if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+        return Response.redirect(new URL('/login', request.url))
+    }
+
+    if (user && request.nextUrl.pathname.startsWith('/login')) {
+        return Response.redirect(new URL('/dashboard', request.url))
+    }
+
     return await updateSession(request)
 }
 
